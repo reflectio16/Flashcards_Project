020a305641650920945c2f4c6de9cb0ec0fa4796
Merve Elma <merelm@gmail.com>
2024-11-11 16:33:22 +0100

    Add quiz ids to topicSlice

--- src/features/topics/topicsSlice.js
+++ src/features/topics/topicsSlice.js
@@ -17,12 +17,21 @@ const topicsSlice = createSlice({
                 icon,
                 quizIds: []
               };
-        }
+        },
+
+        addQuizIdToTopic: (state, action) => {
+            const { id, topicId } = action.payload;
+            
+            // Add the quiz ID to the specified topic's quizIds array
+            if (state.topics[topicId]) {
+              state.topics[topicId].quizIds.push(id);
+            }
+          }
     }
 });
 
 export const selectTopics = (state) => state.topics.topics;
 
-export const { addTopic } = topicsSlice.actions;
+export const { addTopic, addQuizIdToTopic } = topicsSlice.actions;
 export default topicsSlice.reducer;
 
