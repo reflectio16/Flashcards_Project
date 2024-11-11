edbc0d6894ed8be3637545ffa6ca6d73ca6a344c
Merve Elma <merelm@gmail.com>
2024-11-11 16:27:08 +0100

    Create quizzesSlice

---
+++ src/features/quizzes/quizzesSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  quizzes: {}
+};
+
+const quizzesSlice = createSlice({
+  name: 'quizzes',
+  initialState,
+  reducers: {
+    addQuiz: (state, action) => {
+      const { id, name, topicId, cardIds } = action.payload;
+      state.quizzes[id] = { id, name, topicId, cardIds };
+    }
+  }
+});
+
+// Selector to get all quizzes
+export const selectQuizzes = (state) => state.quizzes.quizzes;
+
+
+export const { addQuiz } = quizzesSlice.actions;
+export default quizzesSlice.reducer;
