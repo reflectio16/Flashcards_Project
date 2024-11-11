03d0a45fbccb360b6b5c88919db757d9512acd46
Merve Elma <merelm@gmail.com>
2024-11-11 15:55:15 +0100

    Hook new topic form

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
-import topicsSlice from "../features/topics/topicsSlice";
-// import reducers
-import topicsSlice from "../features/topics/topicsSlice";
+import topicsReducer from "../features/topics/topicsSlice";  
 
 export default configureStore({
   reducer: {
-    
+    topics: topicsReducer,  
   },
 });
+
