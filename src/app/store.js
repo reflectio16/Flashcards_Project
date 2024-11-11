86154aaffffaa2463a72f3e71ea5c03d8de3f597
Merve Elma <merelm@gmail.com>
2024-11-11 15:32:56 +0100

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import topicsSlice from "../features/topics/topicsSlice";
+// import reducers
+import topicsSlice from "../features/topics/topicsSlice";
+
+export default configureStore({
+  reducer: {
+    
+  },
+});
